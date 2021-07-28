0a44fe73540018cc0feb4907f083b681df4530aa
CyrusWarner <cyruswarner27@gmail.com>
2021-07-28 02:45:28 -0500

    Changes to replies serializer added comment foreign key to serializer.

--- youtube_clone/repliesapp/serializers.py
+++ youtube_clone/repliesapp/serializers.py
@@ -5,5 +5,5 @@ from .models import Replies
 class RepliesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Replies
-        fields = ["comment_id", "video_id", "likes", "dislikes", "comment_text"]
+        fields = ["comment", "video_id", "likes", "dislikes", "comment_text"]
 
--- youtube_clone/repliesapp/views.py
+++ youtube_clone/repliesapp/views.py
@@ -16,7 +16,7 @@ class PostReply(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RepliesList(APIView):
